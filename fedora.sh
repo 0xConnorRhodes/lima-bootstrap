32ba28e318f997b8b69d7b8fd459e9967029fe16
Connor Rhodes <connor@rhodes.contact>
2022-12-29 15:14:07 -0600

    added rpmfusion and nonfree to fedora bootstrap script

--- fedora.sh
+++ fedora.sh
@@ -2,6 +2,15 @@
 
 sudo dnf update -y
 
+# enable RPM Fusion
+sudo dnf install \
+  https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+
+# enable RPM Fusion nonfree
+sudo dnf install \
+  https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+
 sudo dnf install -y \
 	python3-pip \
 	fish \
