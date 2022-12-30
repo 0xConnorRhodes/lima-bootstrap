0850ad1c50f0cba54766d55b7beb2c43a36c1b07
Connor Rhodes <connor@rhodes.contact>
2022-12-30 15:41:44 -0600

    added code to pull the toolbox cloud image and stop the initial container

--- fedora/base.sh
+++ fedora/base.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
-sudo dnf update -y
+sudo dnf update -ydfasdf
 
 
 sudo dnf install -y \
 	toolbox \
+
+
+# create an initial toolbox to pull the base cloud image
+toolbox create -y
+
+TOOLBOX_NAME=$(toolbox list | tail -n 1 | awk '{print $2}')
+
+podman stop $TOOLBOX_NAME
